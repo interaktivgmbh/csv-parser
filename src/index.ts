5250c9e6546adb04500849b5a9adc667b25b204f
Mike Kühnapfel <kuehnapfel@interaktiv.de>
2022-08-26 15:10:31 +0200

    add callback functions to parse strings from CSV

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export interface CsvParserOptions {
+export declare interface CsvParserOptions {
   /**
    * Defines the separator to use
    * @default ','
@@ -18,16 +18,74 @@ export interface CsvParserOptions {
   stringDelimiter?: '\'' | '"'
 }
 
+export declare interface CsvCellMetadata {
+  readonly columnIndex: number
+  readonly columnName: string
+  readonly rowIndex: number
+  readonly cellReferenceA1: string
+  readonly cellReferenceRC: string
+}
+
+class CsvCellMetadataClass implements CsvCellMetadata {
+  constructor (
+    readonly columnIndex: number,
+    readonly columnName: string,
+    readonly rowIndex: number
+  ) {}
+
+  get cellReferenceA1 (): string {
+    return `${CsvCellMetadataClass.#convertNumberToLetter(this.columnIndex)}${this.rowIndex}`
+  }
+
+  get cellReferenceRC (): string {
+    return `R${this.rowIndex}C${this.columnIndex}`
+  }
+
+  static #letterArray: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] = [
+    'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ]
+
+  static #convertNumberToLetter (num: number): string {
+    if (num === 1) return 'A'
+
+    const out: number[] = []
+
+    num--
+
+    while (num > 0) {
+      out.unshift(num % 26)
+      num = Math.floor(num / 26)
+    }
+
+    return out
+      .map((it, idx, { length }) => CsvCellMetadataClass.#letterArray[
+        length > 1 && idx < length - 1
+          ? it - 1
+          : it
+      ])
+      .join('')
+  }
+}
+
 /**
  * Simple CSV parse for JavaScript.
  * @param csv Input CSV data
  * @param options Parsing options
+ * @param callbacks Object of optional callback functions to further parse CSV data.
+ *                  Keys are column indexes or column header names.
  * @returns Promise of an array of objects resembling a line of CSV data
  * @async
  */
-export default async function parseCSV<R extends Array<Record<any, string>>> (
+export default async function parseCSV<R extends Array<Record<any, any>>> (
   csv: string,
-  { separator = ',', header = true, stringDelimiter = '"' }: CsvParserOptions = {}
+  { separator = ',', header = true, stringDelimiter = '"' }: CsvParserOptions = {},
+  callbacks: Record<number|string, (str: string, metadata: CsvCellMetadata) => any> = {}
 ): Promise<R> {
   return await new Promise<R>((resolve, reject) => {
     const lines = csv.split(/\r?\n/g)
@@ -75,8 +133,15 @@ export default async function parseCSV<R extends Array<Record<any, string>>> (
     if (header === true) cells.shift()
 
     resolve(
-      cells.map(line => Object.fromEntries(
-        headers.map((key, idx) => [key, line[idx]])
+      cells.map((line, rowIdx) => Object.fromEntries(
+        headers.map((colName, colIdx) => [
+          colName,
+          colName in callbacks
+            ? callbacks[colName](line[colIdx], new CsvCellMetadataClass(colIdx + 1, colName, rowIdx + 1))
+            : colIdx in callbacks
+              ? callbacks[colIdx](line[colIdx], new CsvCellMetadataClass(colIdx + 1, colName, rowIdx + 1))
+              : line[colIdx]
+        ])
       )) as R
     )
   })
